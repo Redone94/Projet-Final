53e782d523e6ebef6e6c4a7f97489ea1f989bc6b
Briffault <banonmagali@yahoo.fr>
2021-03-01 17:27:27 +0100

    commit init

--- ProjetScolaire -back/src/main/java/ProjetScolaire/service/InitDataBaseUser.java
+++ ProjetScolaire -back/src/main/java/ProjetScolaire/service/InitDataBaseUser.java
@@ -1,5 +1,54 @@
 package ProjetScolaire.service;
 
-public class InitDataBaseUser {
+import java.util.List;
 
+import org.hibernate.tool.hbm2ddl.ImportSqlCommandExtractor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import ProjetScolaire.entity.Classe;
+import ProjetScolaire.entity.Matiere;
+import ProjetScolaire.repository.ClasseRepository;
+import ProjetScolaire.repository.MatiereRepository;
+import ProjetScolaire.repository.ProfesseurRepository;
+
+@Service
+public class InitDataBaseUser implements CommandLineRunner{
+	
+	@Autowired
+	private MatiereRepository matiereRepo;
+	
+	@Autowired
+	private ClasseRepository classeRepo;
+
+	
+	@Autowired
+	private ProfesseurRepository profRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncorder;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("test");
+	Matiere m = new Matiere();
+		m.setNomMatiere("mathematiques");
+		m.setCouleur("rouge");
+		
+	Classe c=new Classe();
+	c.setCours(null);
+	c.setNom("5eme D");
+	c.setProfPrincipal("Mme Dupont");
+	
+			
+			matiereRepo.save(m);
+			classeRepo.save(c);
+			
+			
+
+		}
+		
 }
