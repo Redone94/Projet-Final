166464ada4f8f5a4bb11966913afb29b31f34cb1
Asti31 <vanessa.desmartin@gmail.com>
2021-03-04 23:22:02 +0100

    front

--- projet-scolaire-front/src/app/sidebar/sidebar.component.ts
+++ projet-scolaire-front/src/app/sidebar/sidebar.component.ts
@@ -9,6 +9,7 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { Router } from '@angular/router';
 
 import * as $ from 'jquery';
 
@@ -25,13 +26,26 @@ import * as $ from 'jquery';
   ],
 })
 export class SidebarComponent implements OnInit {
-  constructor() {}
-
+  _user: string = '';
+  constructor(private router: Router) {}
   ngOnInit() {
     //Toggle Click Function
     $('#menu-toggle').click(function (e) {
       e.preventDefault();
       $('#wrapper').toggleClass('toggled');
     });
+  }
+
+  public logout() {
+    sessionStorage.removeItem('tokenId');
+    sessionStorage.removeItem('login');
+    this._user = '';
+    this.router.navigate(['/login']);
+  }
+  public user() {
+    if (sessionStorage.getItem('tokenId')) {
+      return true;
+    }
+    return false;
   }
 }
